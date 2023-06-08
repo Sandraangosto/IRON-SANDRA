f24fde85ea339b54cd0743732b34e39d3219be7f
Sandraangosto <sandra_angosto@icloud.com>
2023-06-08 16:46:32 +0200

    cfu 1 vue

---
+++ CFU/CFU SEMANA 9/my-first-vue-project/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import ShortBiography from "./components/ShortBiography.vue" 
+    </script>
+
+<template>
+    <ShortBiography/>
+  <h1>hello word</h1>
+</template>
+
+<style scoped>
