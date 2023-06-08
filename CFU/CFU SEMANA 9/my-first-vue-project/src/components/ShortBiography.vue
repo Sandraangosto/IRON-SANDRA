62e5f7d8c12eef07200148112f528b9c3ab5ba3b
Sandraangosto <sandra_angosto@icloud.com>
2023-06-08 16:46:48 +0200

    cfu vue 1

--- CFU/CFU SEMANA 9/my-first-vue-project/src/components/ShortBiography.vue
+++ CFU/CFU SEMANA 9/my-first-vue-project/src/components/ShortBiography.vue
@@ -3,7 +3,9 @@
         <h1>Mi nombre es Michael Scott</h1>
         <p>Nací en Scranton, Pennsylvania, EE.UU. el 15 de marzo de 1965.</p>
         <p>Me gusta gastar bromas constantes a mis compañeros de trabajo y me encanta ser el centro de atención.</p>
-        <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png" alt="Image of Michael Scott" width="300" height="300">
+        <img src="https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png" 
+        alt="Image of Michael Scott" width="300" 
+        height="300">
 
     </div>
 </template>
