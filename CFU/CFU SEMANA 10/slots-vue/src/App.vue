ca07d30b3fda597cdb5eb87ca6a3e809f2059622
Sandraangosto <sandra_angosto@icloud.com>
2023-06-12 10:52:52 +0200

    cfu semana 10 vue

---
+++ CFU/CFU SEMANA 10/slots-vue/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from "./Card.vue";
+let title = "Here is a title";
+</script>
+
+<style>
+:root {
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+</style>
+
+<template>
+  <Card>
+    <template #header>
+      <h1>{{ title }}</h1>
+    </template>
+    <template #default>
+      <p>Here is some content</p>
+    </template>
+  </Card>
+</template>
---
+++ CFU/CFU SEMANA 10/slots-vue/src/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <article class="container">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+  </article>
+</template>
+
+<style>
+.container {
+  width: 20rem;
+  background: lightgray;
+}
+ 
+header {
+  color: blue;
+}
+</style>
