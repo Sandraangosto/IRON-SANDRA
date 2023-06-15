99d327f86875030e36ae95897fc34e673c441116
Sandraangosto <sandra_angosto@icloud.com>
2023-06-15 11:07:09 +0200

    cfu vue

---
+++ CFU/CFU SEMANA 10/whatchers-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form>
+      <input type="text" v-model.lazy="inputValue" placeholder="Escribe algo.." />
+      <button @click="inputValue = ''">Reset</button>
+    </form>
+  </div>
+</template>
+ 
+<script setup>
+import { ref, watch } from "vue";
+ 
+const inputValue = ref("");
+watch(inputValue, (newValue) => {
+  console.log(`New value: ${newValue}`);
+  alert(`New value: ${newValue}`);
+  return { inputValue };
+});
+</script>
