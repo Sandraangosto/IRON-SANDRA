f7574eb3379ed7fa8b712374a8fe43e3dda505a4
Sandraangosto <sandra_angosto@icloud.com>
2023-07-03 17:43:02 +0200

    cfu semana 11

---
+++ CFU/CFU SEMANA 11/api-simulada-cfu/src/Mirage.vue
@@ -0,0 +1,15 @@
+import { createServer, Model } from 'miragejs';
+
+export function makeServer ({ environment = 'development' } = {}) {
+  const server = createServer({
+    environment,
+    routes() // our routes go here
+  })
+}
+
+
+routes() {
+  this.namespace = 'api';
+  const url = 'https://api.local'; // this is our fake api url which we will use later
+},
+
