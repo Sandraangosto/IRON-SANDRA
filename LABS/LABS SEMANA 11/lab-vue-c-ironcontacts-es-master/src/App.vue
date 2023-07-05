d5eade5bfb72b4785006d66c41e819311424b8db
Sandraangosto <sandra_angosto@icloud.com>
2023-07-05 19:52:09 +0200

    cambios

--- LABS/LABS SEMANA 11/lab-vue-c-ironcontacts-es-master/src/App.vue
+++ LABS/LABS SEMANA 11/lab-vue-c-ironcontacts-es-master/src/App.vue
@@ -1,10 +1,19 @@
-
 <template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <script setup>
-import contacts from "./contacts.json";
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
